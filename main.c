554c39152a7d3cb087b5bfd89d3958d6233f1d7b
Wasiq-Rahman <72743676+Wasiq-Rahman@users.noreply.github.com>
2020-10-15 21:22:40 +0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main()
+{
+    int a;
+    int result=0;
+    printf("Enter the value of A\n");
+    scanf("%d",&a);
+    result = a+2;
+    printf("The result is %d\n",result);
+    return 0;
+}
